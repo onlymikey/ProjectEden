07e03e4a38b85ea461636ddf539d0a0fa3c667ac
onlymikey <angel.lopezb@proton.me>
2025-05-02 17:47:54 -0500

    absproxy for codeserver

--- project-eden/vite.config.ts
+++ project-eden/vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/absproxy/5173/', // <- Añade esto
   plugins: [react(),
   ],
-  
+  server: {
+    port: 5173,
+    allowedHosts: ['.onlymikey.dev'],
+  }
 })
